f07a1dd1d48969d87c61b24bffc8dc96d6a720c5
PRESTEBAN <estebanpesantez22@gmail.com>
2025-06-11 08:45:34 -0500

    "FIX: Añadi el metodo equals"

--- src/App.java
+++ src/App.java
@@ -1,9 +1,5 @@
-import models.Contact;
-import views.*;
-import java.util.Scanner;
-import controllers.*;
-
 import controllers.MenuController;
+import models.Contact;
 public class App {
     public static void main(String[] args) throws Exception {
 
@@ -16,6 +12,11 @@ public class App {
         MenuController cs = new MenuController();
         cs.showMenu();
 
+
+        System.out.println(contact1.equals(contact2));
+        System.out.println(contact1.equals(contact3));
+        System.out.println(contact1.equals(contact4));
+        System.out.println(contact1.equals(contact5));
 
         
 
--- src/models/Contact.java
+++ src/models/Contact.java
@@ -31,6 +31,50 @@ public class Contact <T,U>{
                 "Name: " + nameT +
                 ", Phone: " + numPhone;
     }
+/* 
+    @Override
+    public int hashCode() {
+        
+        return super.hashCode();
+    }
+        
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+*/
+
+@Override
+public boolean equals(Object obj) {
+    if (this == obj) {
+        return true; // mismo objeto
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+        return false; // null o diferente clase
+    }
+    Contact<?, ?> other = (Contact<?, ?>) obj;
+
+    if (nameT == null) {
+        if (other.nameT != null) return false;
+    } else if (!nameT.equals(other.nameT)) {
+        return false;
+    }
+
+    if (numPhone == null) {
+        return other.numPhone == null;
+    } else {
+        return numPhone.equals(other.numPhone);
+    }
+}
+
+
+    
+
+
+
+    
 
 
     
